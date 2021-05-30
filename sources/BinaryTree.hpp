0d4ab51880a95ea7cc0f1a14f51eb90e07169b14
adi <73074680+AdiHoftman@users.noreply.github.com>
2021-05-30 23:34:48 +0300

    Add files via upload

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -1,55 +1,212 @@
 #pragma once
+#include <queue>
 #include <iostream>
+
+using namespace std; 
 
 namespace ariel {
 
-    template<typename T> class BinaryTree{
+    template<typename T>
+    class BinaryTree{
         
         private:
             struct Node{
                 T value;
                 Node *left, *right;
                 
-                Node(T &val = 0, Node *l = nullptr, Node *r = nullptr) : value(val), left(l), right(r) {}
+                Node(T val) : value(val), left(nullptr), right(nullptr) {}
 
                 ~Node(){
                     delete left;
                     delete right;
                 }
-            };
 
-            Node *root;
+                Node(Node&& other) noexcept{
+                    value = other.value;
+                    other.value = nullptr;
+
+                    left = other.left;
+                    other.left = nullptr; 
+    
+                    right = other.right;
+                    other.right = nullptr; 
+                }
+
+                Node(Node& other) : value(other.value){
+                    if(other.left != nullptr){left = new Node(*other.left);}
+                    else{left = nullptr;}
+
+                    if(other.right != nullptr){right = new Node(*other.right);}
+                    else{right = nullptr;}
+                }
+
+                Node& operator=(Node other) {
+                    if(this==other){return *this;}
+                    delete right;
+                    delete left;
+
+                    if(left){left=new Node(*other.left);}
+                    else{left=nullptr;}
+
+                    if(right){right=new Node(*other.right);}
+                    else{right=nullptr;}
+
+                    return *this;                  
+                }
+
+                Node& operator=(Node&& other) noexcept{
+
+                    if(left){delete left;}
+                    if(right){delete right;}
+
+                    left=other.left;
+                    right=other.right;
+                    value=other.value;
+
+                    other.left=nullptr;
+                    other.right=nullptr;
+
+                    return *this;
+                }
+            };
+        Node *root;
         
         public:
-            BinaryTree() : root(nullptr) {}
+            BinaryTree(){root = nullptr;}
 
-            ~BinaryTree(){delete root;}
+             BinaryTree(BinaryTree& other){
+                    if(root != nullptr){root = new Node(*other.root);}
+                    else{root = nullptr;}
+                }
+
+            BinaryTree(BinaryTree&& other) noexcept{
+                root  = other.root;
+                other.root = nullptr;
+            }
+
+            ~BinaryTree(){if(root!=nullptr){delete root;}}
 
             BinaryTree &add_root(T val){
+                if(root != nullptr){
+                    root->value = val;
+                    return *this;
+                }
                 root = new Node(val);
                 return *this;
+                
             }
 
-            BinaryTree &add_left(const T &parent, const T &son){
+            Node *isNull(T val) {
+                Node *current = nullptr;
+                for (auto it = begin(); it != end(); ++it) {
+                    if ((*it) == val) {
+                        current = it.node;
+                        break;
+                    }
+                }
+                return current;
+            }
+
+            BinaryTree &add_left(T father, T son){
+                if(root == nullptr){throw invalid_argument("root is null");}
+                Node *n = isNull(father);
+                if (n == nullptr) {throw invalid_argument("add left");}
+                if (n->left != nullptr) {
+                    n->left->value = son;
+                } else {
+                    n->left = new Node(son);
+                }
                 return *this;
             }
 
-            BinaryTree &add_right(const T &parent, const T &son){
+            BinaryTree &add_right(T father, T son){
+                if(root == nullptr){throw invalid_argument("root is null");}
+                Node *n = isNull(father);
+                if (n == nullptr) {
+                    throw invalid_argument("add right");
+                }
+                if (n->right != nullptr) {
+                    n->right->value = son;
+                } else {
+                    Node *no = new Node(son);
+                    n->right = no;
+                }
                 return *this;
             }
 
-            friend std::ostream &operator<<(std::ostream &os, const BinaryTree<T>& binaryTree){
-                os<<"hey";
+            BinaryTree& operator=(const BinaryTree other) {
+                if (root) {delete root;}
+                root  = new Node(*other.root);
+                return *this;
+            }
+            
+            BinaryTree& operator=(BinaryTree && other)noexcept {
+                if (root) {delete root;}
+                root  = other.root;
+                other.root = nullptr;
+                return *this;
+            }
+
+            friend ostream& operator<<(ostream& os, const BinaryTree<T>& t) {
+                os << "hey";
                 return os;
             }
 
-            class Preorder{
-                private:
-                    Node *node;       
+            class Iterator{
 
                 public:
+                    Node *node;
 
-                    Preorder(Node* ptr = nullptr) : node(ptr){}       
+                    queue<Node*> q;
+
+                    void initQueue0(Node* root1){ //Preorder
+                        if(root1 != nullptr){
+                            q.push(root1);
+                            initQueue0(root1->left);
+                            initQueue0(root1->right);
+                        }
+                    }
+
+                    void initQueue1(Node* root1){ //Inorder  
+                        if(root1 != nullptr){ 
+                            initQueue1(root1->left);
+                            q.push(root1);
+                            initQueue1(root1->right);
+                        }
+                    }
+
+                    void initQueue2(Node* root1){
+                        if(root1 != nullptr){ //Postorder
+                            initQueue2(root1->left);
+                            initQueue2(root1->right);
+                            q.push(root1);
+                        }
+                    }    
+                    Iterator(Node* ptr = nullptr): node(ptr){} //Default constructor
+
+                    Iterator(Node* ptr, int n): node(ptr){ //Constructor
+                        if(n == 0){ //Preorder case
+                            initQueue0(ptr);
+                            q.push(nullptr);
+
+                            node = q.front();
+                            q.pop();
+                        }
+
+                        if(n == 1){ //Inorder case
+                            initQueue1(ptr);
+                            q.push(nullptr);
+                            node = q.front();
+                            q.pop();
+                        }
+
+                        if(n == 2){ //Postorder case
+                            initQueue2(ptr);
+                            q.push(nullptr);
+                            node = q.front();
+                            q.pop();
+                        }
+                    }      
             
                     T& operator*() const{
                         return node->value;
@@ -59,98 +216,45 @@ namespace ariel {
                         return &(node->value);
                     }
                     
-                    Preorder& operator++(){
+                    Iterator& operator++(){
+                        if(!q.empty()){
+                            this->node = this->q.front();
+                            this->q.pop();
+                        }
                         return *this;
                     }
 
-                    const Preorder operator++(int){
-                        return *this;
+                    Iterator operator++(int){
+                        Iterator it = *this;
+                        this->node = this->q.front();
+                        this->q.pop();
+                        return it;
                     }
 
-                    bool operator==(const Preorder& p) const{return node == p.node;}
+                    bool operator==(const Iterator& p) const{return node == p.node;}
                     
-                    bool operator!=(const Preorder& p) const{return !(node == p.node);}   
+                    bool operator!=(const Iterator& p) const{return node != p.node;}
 
-            }; 
-
-            class Inorder{
-                private:
-                    Node *node;
-
-                public:
-
-                    Inorder(Node* ptr = nullptr) : node(ptr){}
-                    
-                    T& operator*() const{
-                        return node->value;
-                    }
-
-                    T* operator->() const{
-                        return &(node->value);
-                    }
-                    
-                    Inorder& operator++(){
-                        return *this;
-                    }
-
-                    const Inorder operator++(int){
-                        return *this;
-                    }
-
-                    bool operator==(const Inorder& p) const{return node == p.node;}
-                    bool operator!=(const Inorder& p) const{return node != p.node;}
-                   
+                    Node *get_node() { return node; }
 
 
             };
 
-            class Postorder{
-                private:
-                    Node *node;
- 
+            Iterator begin_preorder(){return Iterator{root, 0};}
 
-                public:
+            Iterator end_preorder(){return Iterator{};} 
 
-                    Postorder(Node* ptr = nullptr) : node(ptr){}     
+            Iterator begin_inorder(){return Iterator{root, 1};}
 
-                    T& operator*() const{
-                        return node->value;
-                    }
+            Iterator end_inorder(){return Iterator{};}
 
-                    T* operator->() const{
-                        return &(node->value);
-                    }
+            Iterator begin_postorder(){return Iterator{root, 2};}
 
-                    Postorder& operator++(){
-                        return *this;
-                    }
+            Iterator end_postorder(){return Iterator{};}
 
-                    const Postorder operator++(int){
-                        return *this;
-                    }
-
-                    bool operator==(const Postorder& p) const{return node == p.node;}
-                    bool operator!=(const Postorder& p) const{return node != p.node;} 
-                    
-
-
-            };
-
-            Preorder begin_preorder(){return Preorder{root};}
-
-            Preorder end_preorder(){return Preorder{root};} 
-
-            Inorder begin_inorder(){return Inorder{root};}
-
-            Inorder end_inorder(){return Inorder{root};}
-
-            Postorder begin_postorder(){return Postorder{root};}
-
-            Postorder end_postorder(){return Postorder{root};}
-
-            Inorder begin(){return Inorder{root};}
+            Iterator begin(){return Iterator{root, 1};}
 			
-            Inorder end(){return Inorder{root};}
+            Iterator end(){return Iterator{};}
 
     };
 };
